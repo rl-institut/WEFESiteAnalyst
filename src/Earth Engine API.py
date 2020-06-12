58bb4d4622ca915be391eded28a9a526038f3f35
SilviaCorigliano <46005345+SilviaCorigliano@users.noreply.github.com>
2020-06-12 17:29:59 +0200

    Files Upload
    Initial upload of py files for downloading data from Earth Engine and OSM

---
+++ src/Earth Engine API.py
@@ -0,0 +1,128 @@
+'''
+Commands to download different raster layers through earth engine.
+Notice there is a dimension limit for some data providers e.g: 33554432 bytes for Copernicus
+Image Collection:
+To filter Image Collection according to date: filter(ee.Filter.date('2015-01-01', '2015-12-31'))
+To get the most recent image from Image Collection: .sort('system:time_start', False).first()
+To get the median value of all the selected layers: collection.reduce(ee.Reducer.median())
+To select only the wanted layer: .select('name_of_layer'). The name_of_layer is reported in the website, and corresponds
+ to the band name of the dataset
+'''
+import ee
+import geopandas as gpd
+from Functions import download_url
+
+
+##### Import layer with region boundaries and extract its extent#####
+Area=gpd.read_file('Input/Namajavira_4326.shp')
+minx, miny, maxx, maxy = Area.geometry.total_bounds
+# Initialize the Earth Engine module.
+ee.Initialize()
+
+
+#########  DEM with 30m resoulution SRTM #########
+# Get a download URL for an image.
+image = ee.Image("USGS/SRTMGL1_003")
+path = image.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+
+download_url(path,'Output\Elevation.zip')
+
+###########   Land cover ############
+#Copernicus Global Land Cover Layers: CGLS-LC100 collection 2. 100m resolution. Ref year 2015
+#download only image related to discrete classification (see in the documentation)
+collection = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V/Global").select('discrete_classification')
+collection=collection.reduce(ee.Reducer.median()) #check other reducers
+
+path = collection.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+
+download_url(path,'Output\LandCoverCopernicus.zip')
+
+# MCD12Q1.006 MODIS Land Cover Type Yearly Global 500m
+collection = ee.ImageCollection("MODIS/006/MCD12Q1").select('LC_Prop1')#need to choose the best one and the wanted year
+
+collection=collection.reduce(ee.Reducer.median())
+path = collection.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+
+
+
+#GlobCover: Global Land Cover Map. Year 2009. 300 m resoulution. year 2001-2016
+image =ee.Image("ESA/GLOBCOVER_L4_200901_200912_V2_3")
+path = image.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+download_url(path,'Output\LandCover.zip')
+######## Climatic variables #############
+#TerraClimate: Monthly Climate and Climatic Water Balance for Global Terrestrial Surfaces, University of Idaho. res 2.5 arcmin
+
+image = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
+
+#WorldClim Climatology V1. Average from 1960 to 1991. Res=30 arc seconds. Mean, min, max temperature. Precipitation
+image =ee.ImageCollection("WORLDCLIM/V1/MONTHLY")
+
+#MOD16A2.006: Terra Net Evapotranspiration 8-Day Global 500m. kg/m^2/8day Derived by Penman Montheit. Value computed over 8 days period
+image = ee.ImageCollection("MODIS/006/MOD16A2")
+
+#WAPOR Actual Evapotranspiration and Interception. Value computed over 10 days period. 0.00223 arc degrees
+ee.ImageCollection("FAO/WAPOR/2/L1_AETI_D")
+
+######## Rivers #######
+#HydroSHEDS layer, with accumulation and basins
+
+
+
+
+########## Population #########
+#worldpop. resolution=100m
+collection = ee.ImageCollection("WorldPop/GP/100m/pop").sort('system:time_start', False)\
+    .first().select('population')
+
+path = collection.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+download_url(path,'Output\Population.zip')
+
+# GHSL: Global Human Settlement Layers, Population Grid 1975-1990-2000-2015 (P2016). 250 m resolution
+collection=ee.ImageCollection("JRC/GHSL/P2016/POP_GPW_GLOBE_V1").sort('system:time_start', False)\
+    .first().select('population_count')
+
+path = collection.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+download_url(path,'Output\Population.zip')
+
+#GPWv411: Basic Demographic Characteristics (Gridded Population of the World Version 4.11). Data from census between 2000 and 2014.
+#1 km resolution.
+collection=ee.ImageCollection("CIESIN/GPWv411/GPW_Basic_Demographic_Characteristics").sort('system:time_start', False)\
+    .first().select('basic_demographic_characteristics')
+
+path = collection.getDownloadUrl({
+    'scale': 30,
+    'crs': 'EPSG:4326',
+    'region': [[minx, miny], [minx, maxy], [maxx, miny], [maxx, maxy]]
+})
+print(path)
+download_url(path,'Output\PopulationCIESIN.zip')
---
+++ src/OSM API.py
@@ -0,0 +1,117 @@
+'''
+The Overpass API of OSM uses Overpass QL or Overpass XML language.
+Python packages have been developed to work as wrapper/ interface and to automatically download data
+
+########OSMNX##########
+OSMNX: https://geoffboeing.com/2016/11/osmnx-python-street-networks/
+    https://github.com/gboeing/osmnx
+    https://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
+it is a package specifically developed for downloading streets networks from OSM
+
+drive - get drivable public streets (but not service roads)
+drive_service - get drivable streets, including service roads
+walk - get all streets and paths that pedestrians can use (this network type ignores one-way directionality)
+bike - get all streets and paths that cyclists can use
+all - download all non-private OSM streets and paths
+all_private - download all OSM streets and paths, including private-access ones
+'''
+import osmnx as ox
+import geopandas as gpd
+# get the streets network from the name of a place
+# place = 'Piedmont, California, USA'
+# G = ox.graph_from_place(place, network_type='drive')
+# ox.save_graph_shapefile(G, filepath='Output/piedmont')
+
+# get the streets network from a bounding box
+Area=gpd.read_file('Input/Namajavira_4326.shp')
+minx, miny, maxx, maxy = Area.geometry.total_bounds
+G = ox.graph_from_bbox(miny, maxy, minx, maxx, network_type='drive')
+ox.save_graph_shapefile(G, filepath='Output/streets')
+
+############ Requests from query ###########
+
+import requests
+import json
+overpass_url = "http://overpass-api.de/api/interpreter"
+overpass_query = """
+[out:json];
+area["ISO3166-1"="DE"][admin_level=2];
+(node["amenity"="biergarten"](area);
+ way["amenity"="biergarten"](area);
+ rel["amenity"="biergarten"](area);
+);
+out center;
+"""
+response = requests.get(overpass_url,
+                        params={'data': overpass_query})
+data = response.json()
+
+import numpy as np
+import matplotlib.pyplot as plt
+# Collect coords into list
+coords = []
+for element in data['elements']:
+  if element['type'] == 'node':
+    lon = element['lon']
+    lat = element['lat']
+    coords.append((lon, lat))
+  elif 'center' in element:
+    lon = element['center']['lon']
+    lat = element['center']['lat']
+    coords.append((lon, lat))
+# Convert coordinates into numpy array
+X = np.array(coords)
+plt.plot(X[:, 0], X[:, 1], 'o')
+plt.title('Biergarten in Germany')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.axis('equal')
+plt.show()
+
+
+
+
+from pandas.io.json import json_normalize
+overpass_query = """
+[out:json];
+(
+ way["power"="line"](6.7499552751, 36.619987291, 18.4802470232, 47.1153931748);
+  way["power"="cable"](6.7499552751, 36.619987291, 18.4802470232, 47.1153931748);
+  way["power"="minor_line"](6.7499552751, 36.619987291, 18.4802470232, 47.1153931748);
+);
+out geom;
+"""
+response = requests.get(overpass_url,
+                        params={'data': overpass_query})
+data = response.json()
+df = json_normalize(data['elements'])
+for element in data['elements']:
+  if element['type'] == 'node':
+    lon = element['lon']
+    lat = element['lat']
+    coords.append((lon, lat))
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+# Collect coords into list
+coords = []
+for element in data['elements']:
+  if element['type'] == 'node':
+    lon = element['lon']
+    lat = element['lat']
+    coords.append((lon, lat))
+  elif 'center' in element:
+    lon = element['center']['lon']
+    lat = element['center']['lat']
+    coords.append((lon, lat))
+# Convert coordinates into numpy array
+X = np.array(coords)
+plt.plot(X[:, 0], X[:, 1], 'o')
+plt.title('Biergarten in Germany')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.axis('equal')
+plt.show()
+
