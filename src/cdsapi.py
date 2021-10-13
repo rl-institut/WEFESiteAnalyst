6bb2c21e75b791f315435c28d238be64e903f9af
Julian Barinton <60258554+JulianBarinton@users.noreply.github.com>
2021-10-13 20:44:24 +0200

    add cdsapi.py

---
+++ src/cdsapi.py
@@ -0,0 +1,21 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'cams-solar-radiation-timeseries',
+    {
+        'sky_type': 'observed_cloud',
+        'location': {
+            'latitude': 47.853,
+            'longitude': 9.136,
+        },
+        'altitude': '-999.',
+        'date': '2017-01-01/2018-12-31',
+        'time_step': '1hour',
+        'time_reference': 'universal_time',
+        'format': 'csv',
+    },
+    'download.csv')
+
+# source: copernicus data store; https://ads.atmosphere.copernicus.eu/cdsapp#!/dataset/cams-solar-radiation-timeseries?tab=form
