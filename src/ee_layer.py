56cff9ef51d3b2b550fbcc030c97e85c3775689c
Julian Fleischmann <60258554+JulianBarinton@users.noreply.github.com>
2022-12-20 22:13:26 +0100

    add method for adding ee_layer to folium maps
    inspired by google developers

---
+++ src/ee_layer.py
@@ -0,0 +1,19 @@
+# this method is inspired by
+# https://developers.google.com/earth-engine/tutorials/community/intro-to-python-api
+
+import folium
+import ee
+
+def add_ee_layer(self, ee_image_object, vis_params, name):
+    """Adds a method for displaying Earth Engine image tiles to folium map."""
+    map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+    folium.raster_layers.TileLayer(
+        tiles=map_id_dict['tile_fetcher'].url_format,
+        attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
+        name=name,
+        overlay=True,
+        control=True
+    ).add_to(self)
+
+# Add Earth Engine drawing method to folium.
+folium.Map.add_ee_layer = add_ee_layer
