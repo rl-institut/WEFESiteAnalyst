91309db0ba031a6c855b6db782922f04429bd216
SilviaCorigliano <46005345+SilviaCorigliano@users.noreply.github.com>
2023-01-16 23:06:44 +0100

    Update OSM API.py

--- src/OSM API.py
+++ src/OSM API.py
@@ -119,3 +119,69 @@ plt.ylabel('Latitude')
 plt.axis('equal')
 plt.show()
 
+
+
+    # # Transmission grid and substations
+    #
+    # overpass_url = "http://overpass-api.de/api/interpreter"
+    # overpass_query = """
+    # [out:json];
+    # (
+    #  way["power"="line"](""" + str(min_y) + ',' + str(min_x) + ',' + str(
+    #     max_y) + ',' + str(max_x) + ''');
+    # );
+    # out geom;
+    # '''
+    # response = requests.get(overpass_url,
+    #                         params={'data': overpass_query})
+    # data = response.json()
+    # df = pd.json_normalize(data['elements'])
+    # if not df.empty:
+    #     df['geometry_new'] = df['geometry']
+    #     for i, row in df.iterrows():
+    #         points = []
+    #         for j in row['geometry']:
+    #             points.append(Point(j['lon'], j['lat']))
+    #         df.loc[i, 'geometry_new'] = LineString(points)
+    #     geo_df = gpd.GeoDataFrame(
+    #         df[['id', 'tags.power', 'tags.voltage', 'geometry_new']],
+    #         geometry='geometry_new', crs='epsg:4326')
+    #     geo_df.to_crs(crs)
+    #     geo_df.to_file('Output/Datasets/transmission_grid.shp')
+    #
+    # else:
+    #     print('ERROR: Transmission grid data could not be downloaded')
+    #
+    # overpass_query = """
+    # [out:json];
+    # (node["power"="substation"](""" + str(min_y) + ',' + str(
+    #     min_x) + ',' + str(max_y) + ',' + str(max_x) + ''');
+    #  way["power"="substation"](''' + str(min_y) + ',' + str(min_x) + ',' + str(
+    #     max_y) + ',' + str(max_x) + ''');
+    # );
+    # out center;
+    # '''
+    # response = requests.get(overpass_url, params={'data': overpass_query})
+    # data = response.json()
+    # df = pd.json_normalize(data['elements'])
+    # if not df.empty:
+    #     # Collect coordinates into list
+    #     coordinates = []
+    #     for element in data['elements']:
+    #         if element['type'] == 'node':
+    #             lon = element['lon']
+    #             lat = element['lat']
+    #             coordinates.append((lon, lat))
+    #         elif 'center' in element:
+    #             lon = element['center']['lon']
+    #             lat = element['center']['lat']
+    #             coordinates.append((lon, lat))
+    #     # Convert coordinates into numpy array
+    #     coordinates = np.array(coordinates)
+    #     geo_df = gpd.GeoDataFrame(
+    #         df.drop('nodes', axis=1), crs='epsg:4326',
+    #         geometry=gpd.points_from_xy(coordinates[:, 0], coordinates[:, 1]))
+    #     geo_df.to_crs(crs)
+    #     geo_df.to_file('Output/Datasets/substations.shp')
+    # else:
+    #     print('ERROR: Substation data could not be downloaded')
