6a20c147d39f139ceec9af0f835bce7450ed51ba
simoneengreen <s.paolacci@engreen.world>
2024-03-26 12:22:38 +0100

    Updated survey to test the tool

--- DAT_src/library.py
+++ DAT_src/library.py
@@ -2,9 +2,9 @@ import json
 
 from DAT_src.kobo_api_access import load_kobo_data
 
-results = load_kobo_data(form_id="aAiiN4e2fEgrATGr3D69iQ")
+results = load_kobo_data(form_id="aFhz5xWryVk68Mn7y978hn")
 
-first_survey = results[0][1]
+first_survey = results[0][0]
 
 with open("first_survey.json", "w") as file:
     json.dump(first_survey, file)
