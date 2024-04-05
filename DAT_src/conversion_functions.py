9a75705eeefed3a10a2d9138df2a37f3b8de7ae5
simoneengreen <s.paolacci@engreen.world>
2024-04-05 15:07:57 +0200

    Adapted variable formats to make them compatible with the RAMP module

---
+++ DAT_src/conversion_functions.py
@@ -0,0 +1,39 @@
+def convert(dictionary):
+    renamed_dict = {}
+    true_keys_list = []
+
+    for index, key in enumerate(dictionary):
+        # Renaming keys as "0", "1", "2", ...
+        renamed_dict[index] = dictionary[key]
+
+        # Appending true keys to the list
+        if dictionary[key] == True:
+            true_keys_list.append(str(index))
+    new_list = [int(x) for x in true_keys_list]
+
+    return new_list
+
+
+def convert_usage_windows(input_dict):
+    usage_windows = []
+    start_time = None
+
+    for window, active in input_dict.items():
+        hour_range = window.split('-')
+        if active:
+            if start_time is None:
+                start_time = int(hour_range[0])
+        elif start_time is not None:
+            end_time = int(hour_range[0])
+            usage_windows.append([start_time, end_time])
+            start_time = None
+    if start_time is not None:
+        # If there's an active window at the end of the day
+        # assume it ends at midnight (24)
+        usage_windows.append([start_time, 24])
+
+    return usage_windows
+
+
+
+
