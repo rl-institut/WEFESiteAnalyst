9d995fd11ab72991969f6147ad93ead356281b6d
simoneengreen <s.paolacci@engreen.world>
2024-04-29 11:21:13 +0200

    Update defaults.py file

--- DAT_src/preprocessing/defaults.py
+++ DAT_src/preprocessing/defaults.py
@@ -6,3 +6,43 @@ usage_wd_defaults = {
     '18-22' : None,
     '22-24' : None
 }
+
+months_present_defaults = {
+    'January' : None,
+    'February' : None,
+    'March' : None,
+    'April' : None,
+    'May' : None,
+    'June' : None,
+    'July' : None,
+    'August' : None,
+    'September' : None,
+    'October' : None,
+    'November' : None,
+    'December' : None
+}
+
+working_day_default = {
+    'Monday' : None,
+    'Tuesday' : None,
+    'Wednesday' : None,
+    'Thursday' : None,
+    'Friday' : None,
+    'Saturday' : None,
+    'Sunday' : None
+}
+
+months_defaults = {
+    '1' : None,
+    '2' : None,
+    '3' : None,
+    '4' : None,
+    '5' : None,
+    '6' : None,
+    '7' : None,
+    '8' : None,
+    '9' : None,
+    '10' : None,
+    '11' : None,
+    '12' : None
+}
